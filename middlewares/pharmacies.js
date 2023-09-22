6e78dc7eb07239710a1fbfd1f65d7ee7c719e95f
matteo lanci <m.lanci@live.com>
2023-09-22 21:31:15 +0200

    EDIT: pharmacies midw.

--- middlewares/pharmacies.js
+++ middlewares/pharmacies.js
@@ -1,19 +1,8 @@
 const puppeteer = require("puppeteer");
-const proxy = "";
 
 const pharmacies = async (locationName) => {
-  const browser = await puppeteer.launch({ headless: "new", args: [`--proxy-server=${proxy}`] });
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-
-  await page.setExtraHTTPHeaders({
-    "user-agent":
-      "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    "upgrade-insecure-requests": "1",
-    accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9,en;q=0.8",
-  });
 
   const url = `https://www.paginegialle.it/farmacie-turno/${locationName}`;
 
